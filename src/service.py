17453ea0ba4d0cccb5454f4fedf53cccd18fef16
arkady <arkady>
2021-05-13 22:44:09 +0300

    service

--- src/service.py
+++ src/service.py
@@ -1,19 +1,23 @@
 from urllib.parse import urlparse, parse_qs
 import logging
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-from threading import Semaphore
-from time import time
-from os.environ import environ
+from threading import Semaphore, Thread
+from time import time, sleep
+from os import environ
+from dataclasses import dataclass
 
 
+@dataclass
 class Statistics:
-    def __init__(self):
-        self.acquire_latency = 0.0
-        self.acquire_latency_max = 0.0
-        self.acquire_failed = 0
-        self.resp_400 = 0
-        self.acquire_pending_max = 0
-        self.acquire_pending = 0
+    timer_1s: int = 0
+    acquire_latency: float = 0
+    acquire_latency_max: float = 0
+    acquire_failed: int = 0
+    resp_200: int = 0
+    resp_400: int = 0
+    acquire_pending_max: int = 0
+    acquire_pending: int = 0
+    unknow_post: int = 0
 
 
 _statistics = Statistics()
@@ -45,11 +49,25 @@ class HeadlessnessServer(BaseHTTPRequestHandler):
     # default process at most 1 query
     _throttle_max, _throttle = 1, Semaphore(1)
 
-    def _400(self):
-        self.send_response(400)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        _statistics.resp_400 += 1
+    def _400(self, msg):
+        try:
+            self.send_response(400)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(msg.encode("utf-8"))
+            _statistics.resp_400 += 1
+        except Exception as e:
+            self._logger.error(f"Faied to write to remote {e}")
+
+    def _200(self, msg):
+        try:
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(msg.encode("utf-8"))
+            _statistics.resp_200 += 1
+        except Exception as e:
+            self._logger.error(f"Faied to write to remote {e}")
 
     def _check_throttle(self):
         time_start = time()
@@ -57,12 +75,9 @@ class HeadlessnessServer(BaseHTTPRequestHandler):
             _statistics.acquire_failed += 1
             err_msg = "Too many requests"
             self._logger.error(err_msg)
-            try:
-                self._400()
-                self.wfile.write(err_msg.encode("utf-8"))
-            except Exception as e:
-                self._logger.error(f"Faied to write to remote {e}")
+            self._400(err_msg)
             return False
+
         elapsed_time = time() - time_start
         _statistics.acquire_latency = elapsed_time
         _statistics.acquire_latency_max = max(
@@ -87,6 +102,19 @@ class HeadlessnessServer(BaseHTTPRequestHandler):
         )
         if not self._check_throttle():
             return
+        if parsed_url.path not in ["/fetch"]:
+            _statistics.unknow_post += 1
+            err_msg = f"Unknown post {parsed_url.path}"
+            self._logger.error(err_msg)
+            self._400()
+            self._write_to_remote(err_msg)
+            return
+        self._200("Ok")
+
+
+def shutdown():
+    global is_running
+    is_running = False
 
 
 def main():
@@ -102,8 +130,16 @@ def main():
     http_server = ThreadingHTTPServer(
         (http_interface, http_port), HeadlessnessServer(logger)
     )
-    http_server.serve_forever()
+    http_server_thread = Thread(target=http_server.serve_forever)
+    http_server_thread.start()
+    return http_server_thread, http_server
 
 
 if __name__ == "__main__":
-    main()
+    is_running = True
+    http_server_thread, http_server = main()
+    while is_running:
+        sleep(1.0)
+        _statistics.timer_1s += 1
+    http_server.shutdown()
+    http_server_thread.join()
---
+++ src/test_service.py
@@ -0,0 +1,36 @@
+import pytest
+from service import main
+from requests import post
+import traceback
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_keyboard_interrupt(excinfo):
+    traceback.format_exc()
+
+
+def setup_module(module):
+    module.http_server_thread, module.http_server = main()
+
+
+def teardown_module(module):
+    print(
+        "Tearing down ...",
+    )
+    module.http_server.shutdown()
+    module.http_server_thread.join()
+    print("completed")
+
+
+def test_post():
+    url = "http://0.0.0.0:8081/fetch?ur=http%3A%2F%2Fgoogle.com"
+    request_result = post(url)
+    assert (
+        request_result.status_code == 200
+    ), f"Got response for {url} {request_result.status_code} {request_result.text}"
+
+    url = "http://0.0.0.0:8081/futch?ur=http%3A%2F%2Fgoogle.com"
+    request_result = post(url)
+    assert (
+        request_result.status_code == 400
+    ), f"Got response for {url} {request_result.status_code} {request_result.text}"
